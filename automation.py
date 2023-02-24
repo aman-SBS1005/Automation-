512b44bfd45fb2e11396203e13efa56bc175190a
aman-SBS1005 <77289565+aman-SBS1005@users.noreply.github.com>
2023-02-25 00:01:25 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,12 +1,13 @@
 import openpyxl as xl
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
-cell = sheet['a1']
-cell = sheet.cell(1, 1)
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    corrected_price = cell.value * 0.9
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
-wb.save('transactions2.xlsx')
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+    wb.save(filename)
+
